fdc37221452f20625d79ffb646838108df78e3a7
Magicdonutman <stevoth@hotmail.com>
2016-10-17 17:46:05 -0500

    Add files via upload
    Updated after video finished

--- Program.cs
+++ Program.cs
@@ -12,14 +12,17 @@ namespace Module07_StevenVoth
         {
             Car myCar = new Car();
 
+            Car.MyMethod();
             myCar.Make = "Oldsmobile";
             myCar.Model = "Cutlass Supreme";
             myCar.Year = 1986;
             myCar.Color = "Silver";
-
             Car myOtherCar;
+
             myOtherCar = myCar;
 
+            Car myThirdCar = new Car("Ford", "F150", 1998, "Black");
+            Console.WriteLine(myThirdCar);
             Console.WriteLine("{0} {1} {2} {3}", 
                 myOtherCar.Make, 
                 myOtherCar.Model,
@@ -34,16 +37,46 @@ namespace Module07_StevenVoth
               myCar.Year,
               myCar.Color);
 
+            myOtherCar = null;
+
+            /*
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+            */
+            myCar = null;
+
             Console.ReadLine();
         }
     }
-    
+
     class Car
     {
-        public string Make { get; set;}
-        public string Model { get; set;}
-        public int Year { get; set;}
-        public string Color { get; set;}
+
+        public Car()
+        {
+            this.Make = "Nissan";
+
+        }
+        public Car(string make, string model, int year, string color)
+        {
+            Make = Make;
+            Model = Model;
+            year = Year;
+            color = Color;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Hello from my Static Method!");
+        }
     }
 
 
